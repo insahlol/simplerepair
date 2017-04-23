f6b71bf99ce96dc577b4f5694c4a2df3e2a24dc7
insah <mikehollasch@gmail.com>
2017-04-23 00:33:44 -0400

    Commit project

---
+++ src/main/java/co.insah.simplerepair/SimpleRepair.java
@@ -0,0 +1,41 @@
+package co.insah.simplerepair;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Repairable;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleRepair extends JavaPlugin {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) return false;
+
+        Player player = (Player) sender;
+        if (command.getName().equalsIgnoreCase("repair")) {
+            if (!player.hasPermission("simplerepair.use")) {
+                player.sendMessage(ChatColor.RED + "You do not have permission to perform this command.");
+                return true;
+            }
+            repairAll(player);
+            player.sendMessage(ChatColor.GREEN + "You have repaired your tools and armor!");
+            return true;
+        }
+        return false;
+    }
+
+    private void repairAll(Player player) {
+        for (ItemStack items : player.getInventory().getContents()) {
+            if (items instanceof Repairable) {
+                items.setDurability((short)0);
+            }
+        }
+        for (ItemStack items : player.getInventory().getArmorContents()) {
+            if (items instanceof Repairable) {
+                items.setDurability((short)0);
+            }
+        }
+    }
+}
